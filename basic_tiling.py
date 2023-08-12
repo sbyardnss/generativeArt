d7cb4c2f9d6ed14b00d69a9db07a65b6114ed41b
Mary Williams <sbyardnss@gmail.com>
2023-08-12 11:22:43 -0500

    tiling completed

--- basic_tiling.py
+++ basic_tiling.py
@@ -1,13 +1,17 @@
-from turtle import *
+import uuid
 import random
 import tkinter as tk
-
+from turtle import *
+from svg_turtle import SvgTurtle
+from PIL import Image
 # turtle.setup(1000, 1000)
-
-setup(1000, 1000)
+width = 1000
+height = 1000
+setup(width, height)
 # speed('fastest')
 
-tracer(False)
+# tracer(False)
+
 
 def tiling(x, y, s, l, mode="straight"):
     # we have reached the final level of recursion
@@ -48,11 +52,23 @@ def tiling(x, y, s, l, mode="straight"):
         tiling(x-s, y-s, s, l, mode)
         tiling(x+s, y-s, s, l, mode)
 
+# width(2)
+
+
 hideturtle()
-width(2)
 tracer(False)
+# speed('fastest')
 # dont go past 7 levels
-tiling(0,0,400,5, 'diagonal')
-# update()
+tiling(0, 0, 400, 5, 'diagonal')
 tracer(True)
+file_name = uuid.uuid1()
+getscreen().getcanvas().postscript(file=f'output/tiling/tiling{file_name}.eps')
+# ps_image = Image.open('output/tiling80927ee0-388d-11ee-9a50-8c859040d697.ps')
+# ps_image.save('output/tiling80927ee0-388d-11ee-9a50-8c859040d697.png')
 exitonclick()
+# def write_file(function, file, w, h):
+#     t = SvgTurtle(w, h)
+#     function(t)
+#     t.save_as(file)
+
+# write_file(tiling(0,0,400,5, 'diagonal'), f'{file_name}.svg', width, height)
