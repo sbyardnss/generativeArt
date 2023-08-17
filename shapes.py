766297aa616fc62e1c21c7a9fa1ea2797dcb603e
Mary Williams <sbyardnss@gmail.com>
2023-08-17 15:32:46 -0500

    messed with coloring on shapes

--- shapes.py
+++ shapes.py
@@ -12,20 +12,22 @@ def draw_square(size):
     for i in range(4):
         forward(size)
         right(90)
-def draw_triangle(size):
+def draw_triangle(size, angle_bool):
     angle = random.randint(0,size)
-    # right(angle)
+    if angle_bool == True:
+        right(angle)
     for i in range(3):
         forward(size)
         right(120)
-    # left(angle)
+    if angle_bool == True:
+        left(angle)
 
 def make_shapes(size):
     penup()
     # goto(-100, 100)
     for y in range(400, -400, -size):
         for x in range(-400, 400, size):
-            num = random.randint(0, 3)
+            num = random.randint(0, 2)
             begin_fill()
             if num == 0:
                 goto(x+size*.5,y)
@@ -33,20 +35,38 @@ def make_shapes(size):
             elif num ==1:
                 goto(x,y)
                 draw_square(size)
+            # elif num ==3:
+            #     None
             else:
                 goto(x,y-.1*size)
-                draw_triangle(size)
+                if y < 100:
+                    draw_triangle(size, True)
+                else:
+                    draw_triangle(size, False)
             # fillcolor(fc[num])
             if num ==2:
-                fillcolor('maroon')
+                if y < -100:
+                    fillcolor(fc[0])
+                elif y < 0:
+                    fillcolor(fc[1])
+                elif y < 100:
+                    fillcolor(fc[2])
+                elif y< 200:
+                    fillcolor(fc[3])
+                elif y< 300:
+                    fillcolor(fc[4])
+                else:
+                    fillcolor(fc[5])
+                # fillcolor('maroon')
             elif num ==1:
                 fillcolor('teal')
             else:
+                print(num)
                 fillcolor('black')
             end_fill()
 make_shapes(40)
 
 
 tracer(True)
-create_file('shapes')
+# create_file('shapes')
 exitonclick()
